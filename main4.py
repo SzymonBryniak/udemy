c0ec4cba4b654587f6f72d9f5eac01f64c6f7ac1
SzymonBryniak <szymonbryniakproject@gmail.com>
2024-04-01 15:38:05 +0100

    random walk, random color update

--- main4.py
+++ main4.py
@@ -1,10 +1,19 @@
 import turtle as t
 import random
+
 tim = t.Turtle()
 screen = tim.screen
+screen.colormode(255)
 colors = ["blue violet", "indian red", "midnight blue", "orange red", "green", "blue", "medium slate blue", "coral",
               "medium violet red"]
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rgb = r, g, b
+    return tuple(rgb)
+random_color()
 
 def random_move(ranangle, rancolor, tim):
     tim.width(20)
@@ -16,7 +25,7 @@ def random_move(ranangle, rancolor, tim):
 anglelist = [0 ,90, 180, 270]
 for i in range(1000):
     angle = random.choice(anglelist)
-    rancolor = random.choice(colors)
+    rancolor = random_color()
     random_move(angle, rancolor, tim)
     # if angle == angle2:
     # if angle == angle2:
